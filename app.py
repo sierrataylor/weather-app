72cf90e698156132bf9f51a76836f104fcfe8613
sierrataylor <staylor23@jcu.edu>
2021-07-13 20:05:17 -0400

    Updated to make more efficient

--- app.py
+++ app.py
@@ -1,33 +1,27 @@
 from flask import Flask, render_template, request
+import socket
 import requests
-import configparser
+#import configparser
 
 app = Flask(__name__)
 
+api = '3a152a1afe50c56cf61fef12cd96ea2f'
+ip = requests.get('https://api.ipify.org').text
+params = {
+    'access_key': api,
+    'query': ip,
+    'units': 'f'
+}
+
 @app.route("/")
-def home():
-    return render_template("home.html")
+def weather():
+    api_result = requests.get('http://api.weatherstack.com/current', params)
+    json_response = api_result.json()
+    print(json_response)
+    print()
+    location = str(json_response['location']['name'])
+    print(json_response['location']['name']+','+json_response['location']['region'])
+    return render_template('weather.html', location=location)
 
-@app.route("/weather", methods=["GET", "POST"])
-def render_weather_page():
-    zip_code = request.form['zipCode'] 
-    api = get_api()
-    data = get_weather_results(zip_code, api)
-    temp = "{0:.2f}".format(data["main"]["temp"])
-    feels_like = "{0:.2f}".format(data["main"]["feels_like"])
-    weather = data["weather"][0]["main"]
-    location = data["name"] 
-    return render_template("weather.html", location=location, temp=temp, weather=weather, feels_like=feels_like)
-    
-def get_api():
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    return config['openweathermap']['api']
-
-def get_weather_results(zip_code, api):
-    url = "https://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip_code, api)
-    r = requests.get(url)
-    return r.json()
-    
 if __name__ == "__main__":
 	app.run(debug=True)
